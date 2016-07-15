a1a97c4f86c3ec4043730980bd912678c6e57ae9
jekku <jekripreclaroorlina@gmail.com>
2016-07-15 17:41:51 +0800

    Updates - Update error handling and validation

--- app/Http/Controllers/Api/ThirdPartyController.php
+++ app/Http/Controllers/Api/ThirdPartyController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 
 use Input;
+use Validator;
 use App\Rule;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
@@ -20,11 +21,28 @@ class ThirdPartyController extends Controller
     public function postLink () {
         $input = Input::only('short_url', 'long_url');
 
-        Rule::create($input);
+        $validation = Validator::make($input, [
+            'short_url' => 'required|min:2',
+            'long_url' => 'required|max:2083'
+        ]);
+
+        if ($validation->fails()) {
+            return response()->json([
+                $validation->errors()
+            ], 400);
+        }
+
+        try {
+            Rule::create($input);
+        } catch (Exception $ex) {
+            return response()->json([
+                'message' => 'Server failed to create redirect rule'
+            ], 500);
+        };
 
         return response()->json([
-          'message' => 'Successfully Created',
-          'meta' => $input
+            'message' => 'Successfully Created',
+            'meta' => $input
         ]);
     }
 }
